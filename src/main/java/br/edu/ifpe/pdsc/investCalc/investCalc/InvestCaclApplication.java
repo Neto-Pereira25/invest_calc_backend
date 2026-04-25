a9b8bdf107b4fb497fb7a749e4348f27e24d0e26
José Pereira da Silva Neto <devneto203@gmail.com>
2026-04-25 02:25:10 -0300

    feat: create investcalc project

---
+++ src/main/java/br/edu/ifpe/pdsc/investCalc/investCalc/InvestCaclApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.pdsc.investCalc.investCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestCaclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestCaclApplication.class, args);
+	}
+
+}
