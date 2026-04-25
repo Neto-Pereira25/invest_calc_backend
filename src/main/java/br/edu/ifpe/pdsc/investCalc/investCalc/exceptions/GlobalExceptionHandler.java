0e19a92ea9f1007b1b17ba36df14474f64e6415f
José Pereira da Silva Neto <devneto203@gmail.com>
2026-04-25 19:19:43 -0300

    faet: Implement user login test.

--- src/main/java/br/edu/ifpe/pdsc/investCalc/investCalc/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/edu/ifpe/pdsc/investCalc/investCalc/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package br.edu.ifpe.pdsc.investCalc.investCalc.exceptions;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
